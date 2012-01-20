afc7219876baddd75dae027adcd8dc1aeb44fd4a
Alan Aguiar <alanjas@hotmail.com>
2012-01-20 16:59:43 -0200

    initial version based on 3

---
+++ activity.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from olpcgames import activity
+
+class Activity(activity.PyGameActivity):
+	game_name = 'conozcoam'
+	game_title = 'Conozco America'
+	game_size = (1200,900)
