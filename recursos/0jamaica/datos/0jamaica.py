e8ba24503757a2d812b2b3c45e7b3923577a8c11
Alan Aguiar <alanjas@hotmail.com>
2012-04-04 18:15:19 -0300

    fix 0jamaica to the new system

---
+++ recursos/0jamaica/datos/0jamaica.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+NAME = _('Jamaica')
+
+STATES = [
+    (_('Hanover'), 254, 91, 322, 0),
+    (_('Westmoreland'), 253, 101, 368, 0),
+    (_('Saint James'), 252, 191, 292, 90),
+    (_('Trelawny'), 251, 273, 335, 0),
+    (_('Saint Elizabeth'), 250, 228, 445, -70),
+    (_('Manchester'), 249, 312, 450, -60),
+    (_('Saint Ann'), 248, 396, 351, 0),
+    (_('Clarendon'), 247, 390, 462, -60),
+    (_('Saint Catherine'), 246, 478, 447, -60),
+    (_('Saint Mary'), 245, 525, 363, -40),
+    (_('Saint Andrew'), 244, 568, 447, -10),
+    (_('Kingston'), 243, 568, 472, 0),
+    (_('Portland'), 242, 650, 417, 0),
+    (_('Saint Thomas'), 241, 677, 492, 0)
+]
+
+CAPITALS = [
+    (_('Kingston'), 558, 472, 0, 30, -14),
+    (_('Black River'), 193, 451, 1, 0, 14),
+    (_('Falmouth'), 260, 289, 1, 20, -14),
+    (_('Lucea'), 80, 305, 1, 0, -14),
+    (_('Mandeville'), 311, 450, 1, 0, 14),
+    (_('May Pen'), 404, 476, 1, -15, 14),
+    (_('Montego Bay'), 170, 295, 1, 0, -14),
+    (_('Morant Bay'), 693, 502, 1, -10, 14),
+    (_('Port Antonio'), 677, 399, 1, 10, 14),
+    (_('Port Maria'), 523, 329, 1, 45, 0),
+    (_("Saint Ann's Bay"), 418, 307, 1, 0, -14),
+    (_('Savanna-la-Mar'), 93, 385, 1, 20, -14),
+    (_('Spanish Town'), 502, 465, 1, -25, -14)
+]
+
+CITIES = [
+    (_('Albert Town'), 298, 357, 2, 0, 14),
+    (_('Alligator Pond'), 296, 507, 2, 0, 14),
+    (_('Annotto Bay'), 566, 366, 2, 45, -14),
+    (_('Bluefields'), 135, 400, 2, 0, 14),
+    (_('Bog Walk'), 486, 426, 2, 40, 0),
+    (_("Brown's Town"), 358, 323, 2, -20, 14),
+    (_('Buff Way'), 605, 378, 2, 0, 14),
+    (_('Bull Bay'), 602, 479, 2, 25, 14),
+    (_('Christiana'), 318, 402, 2, -30, 14),
+    (_('Duncans'), 302, 295, 2, 0, 14),
+    (_('Frankfield'), 361, 411, 2, 25, 14),
+    (_('Linstead'), 476, 413, 2, 0, -14),
+    (_('Moneague'), 446, 366, 2, 0, 14),
+    (_('Montpelier'), 163, 331, 2, 0, 14),
+    (_('Negril'), 22, 364, 2, 5, -14),
+    (_('Ocho Rios'), 451, 318, 2, 0, 14),
+    (_('Oracabessa'), 503, 320, 2, 30, -14),
+    (_('Port Esquivel'), 443, 499, 2, 10, 14),
+    (_('Port Morant'), 720, 499, 2, 10, -14),
+    (_('Portmore'), 532, 474, 2, 0, 14),
+    (_('Reading'), 159, 307, 2, 0, 14),
+    (_('The Alley'), 395, 533, 2, 0, 14)
+]
+
+RIVERS = [
+    (_('Cabarita River'), 254, 81, 349, 60),
+    (_('Great River'), 253, 149, 346, -60),
+    (_('Montego River'), 252, 189, 291, -30),
+    (_('Martha Brae River'), 251, 270, 320, 60),
+    (_('Black River'), 250, 207, 417, 60),
+    (_('Milk River'), 249, 342, 483, -70),
+    (_('Minho River'), 248, 379, 406, -30),
+    (_("Coleburn's Gully River"), 247, 445, 466, -70),
+    (_('Magno River'), 246, 485, 434, -70),
+    (_('Flint River'), 245, 533, 363, 0),
+    (_('Wag Water River'), 244, 543, 414, -45),
+    (_('Yallahs River'), 243, 605, 468, -60),
+    (_('Morant River'), 242, 682, 482, -60),
+    (_('Negro River'), 241, 648, 485, -45),
+    (_('Plantain Garden River'), 240, 709, 471, -50),
+    (_('Grande River'), 239, 686, 418, -45),
+    (_('Nuevo River'), 238, 462, 345, -60),
+    (_('Cobre River'), 237, 532, 454, -30),
+    (_('Caribbean Sea'), 236, 378, 152, 0)
+]
+
+ROUTES = []
+
+
