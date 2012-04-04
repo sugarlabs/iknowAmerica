6a71a49619ae91cf1a7f7bcfb94cbd0109b28486
Alan Aguiar <alanjas@hotmail.com>
2012-04-04 16:53:39 -0300

    fix 0guyana to the new system

---
+++ recursos/0guyana/datos/0guyana.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+NAME = _('Guyana')
+
+STATES = [
+    (_('Barima-Waini'), 254, 286, 126, 0),
+    (_('Pomeroon-Supenaam'), 253, 398, 161, 45),
+    (_('West-Demerara'), 252, 435, 242, 45),
+    (_('Demerara-Mahaica'), 251, 482, 263, 45),
+    (_('Mahaica-Berbice'), 250, 517, 290, 45),
+    (_('Cuyuni-Mazaruni'), 249, 263, 300, 0),
+    (_('Potaro-Siparuni'), 248, 339, 458, -45),
+    (_('Upper Takutu-Upper Essequibo'), 247, 380, 695, 60),
+    (_('Upper Demerara-Berbice'), 246, 466, 396, 0),
+    (_('East Berbice-Corentyne'), 245, 552, 690, -60),
+    (_('Brazil'), 244, 128, 730, 0),
+    (_('Venezuela'), 243, 93, 187, 0),
+    (_('Suriname'), 242, 673, 536, 0)
+]
+
+CAPITALS = [
+    (_('Georgetown'), 480, 218, 0, 40, -14)
+]
+
+CITIES = [
+    (_('Arimu Mine'), 362, 260, 2, -15, -14),
+    (_('Bartica'), 430, 267, 2, 20, -14),
+    (_('Charity'), 426, 145, 2, 0, -14),
+    (_('Corriverton'), 599, 321, 2, 45, 14),
+    (_('Dadanawa'), 328, 686, 2, 0, 14),
+    (_('Good Hope'), 319, 569, 2, 0, 14),
+    (_('Isherton'), 343, 747, 2, 0, 14),
+    (_('Issano'), 329, 337, 2, 0, 14),
+    (_('Ituni'), 474, 371, 2, -10, -14),
+    (_('Kalkuni'), 522, 346, 2, 30, 0),
+    (_('Kartabu Point'), 413, 270, 2, 10, 14),
+    (_('Kurupukari'), 422, 469, 2, 0, 14),
+    (_('Kwakwani'), 489, 398, 2, 0, 14),
+    (_('Lethem'), 291, 623, 2, 0, 14),
+    (_('Linden'), 467, 311, 2, 20, 14),
+    (_('Mabaruma'), 285, 57, 2, 0, 14),
+    (_('Mahdia'), 365, 404, 2, 0, 14),
+    (_('Mara'), 546, 308, 2, 0, 14),
+    (_('Matthews Ridge'), 240, 138, 2, 0, 14),
+    (_('Orinduik'), 265, 466, 2, 0, 14),
+    (_('Parika'), 449, 214, 2, -15, 14),
+    (_('Peters Mine'), 339, 285, 2, -35, 14),
+    (_('Port Kaituma'), 273, 106, 2, 0, 14),
+    (_('Potaro Landing'), 368, 384, 2, 0, -14),
+    (_('Rockstone'), 435, 313, 2, -30, 14),
+    (_('Rose Hall'), 574, 280, 2, 40, 0),
+    (_('Rosignol'), 552, 277, 2, -20, -14),
+    (_('Spring Garden'), 435, 193, 2, 0, -14),
+    (_('Surama'), 375, 534, 2, 0, 14),
+    (_('Takama'), 508, 362, 2, 25, 14)
+]
+
+RIVERS = [
+    (_('Cuyuni River'), 254, 272, 212, 0),
+    (_('Wenamu River'), 253, 139, 286, 90),
+    (_('Mazaruni River'), 252, 341, 311, 30),
+    (_('Essequibo River'), 251, 401, 474, -45),
+    (_('Kuyuwini River'), 250, 382, 790, 20),
+    (_('Rupununi River'), 249, 311, 720, -80),
+    (_('Rewa River'), 248, 409, 679, -80),
+    (_('Potaro River'), 247, 317, 394, 20),
+    (_('Tacutu River'), 246, 314, 514, -70),
+    (_('Demerara River'), 245, 474, 296, 85),
+    (_('Courantyne River'), 244, 518, 508, 60),
+    (_('Berbice River'), 243, 524, 370, 60),
+    (_('New River'), 242, 539, 756, 55),
+    (_('Amakura River'), 241, 204, 91, 35),
+    (_('Barama River'), 240, 256, 177, 10),
+    (_('Barima River'), 239, 229, 139, 5),
+    (_('Atlantic Ocean'), 238, 581, 101, 0)
+]
+
+ROUTES = []
+
+
