3b1b87d6fc7395d0e394e9c927a8f66034793f59
Alan Aguiar <alanjas@hotmail.com>
2012-04-04 09:38:12 -0300

    fix 0belice to new sistem

---
+++ recursos/0belice/datos/0belice.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+NAME = _('Belice')
+
+STATES = [
+    (_('Orange Walk'), 254, 267, 286, 0),
+    (_('Cayo'), 253, 259, 517, 0),
+    (_('Toledo'), 252, 269, 737, 0),
+    (_('Corozal'), 251, 433, 105, 0),
+    (_('Belice'), 250, 408, 333, 0),
+    (_('Staan Creek'), 249, 398, 560, 45),
+    (_('Mexico'), 248, 160, 91, 0),
+    (_('Guatemala'), 247, 80, 548, 0),
+    (_('Honduras'), 246, 628, 868, 0),
+    (_('Is. Turneffe'), 245, 646, 416, 60)
+]
+
+CAPITALS = [
+    (_('Belmopan'), 308, 429, 0, 20, 14),
+    (_('Belize'), 489, 352, 1, 20, 14),
+    (_('Corozal'), 427, 51, 1, 25, -14),
+    (_('Dangriga'), 480, 524, 1, 30, 14),
+    (_('Orange Walk'), 374, 157, 1, 30, 14),
+    (_('Punta Gorda'), 294, 807, 1, 40, 14),
+    (_('San Ignacio'), 211, 460, 1, 0, -14)
+]
+
+CITIES = [
+    (_('August Pine Ridge'), 321, 193, 2, -55, -14),
+    (_('Barranco'), 257, 839, 2, 0, 14),
+    (_('Benque Viejo del Carmen'), 189, 487, 2, -60, 14),
+    (_('Bermudian Landing'), 383, 329, 2, -30, 14),
+    (_('Big Creek'), 421, 671, 2, 35, 14),
+    (_('Big Fall'), 269, 757, 2, 10, 14),
+    (_('Blue Creek Village'), 276, 220, 2, -25, 14),
+    (_('Buena Vista'), 385, 105, 2, -60, 0),
+    (_('Burrell Boom'), 424, 325, 2, -30, -14),
+    (_('Crooked Tree'), 382, 257, 2, 30, 14),
+    (_('Gales Point'), 446, 453, 2, 10, -14),
+    (_('Gallon Jug'), 219, 328, 2, 0, -14),
+    (_('Guinea Grass'), 362, 196, 2, 60, 0),
+    (_('Hattieville'), 427, 359, 2, 0, 14),
+    (_('Hopkins'), 458, 558, 2, 30, 14),
+    (_('Independence'), 419, 665, 2, -35, -14),
+    (_('Jalacte'), 168, 774, 2, 0, 14),
+    (_('La Democracia'), 377, 401, 2, 35, 14),
+    (_('Ladyville'), 456, 331, 2, 30, -14),
+    (_('Libertad'), 408, 83, 2, 35, -14),
+    (_('Little Belize'), 422, 116, 2, 55, 0),
+    (_('Maskall'), 454, 226, 2, 10, -14),
+    (_('Middlesex'), 393, 506, 2, 0, 14),
+    (_('Monkey River Town'), 394, 722, 2, 40, 14),
+    (_('Mullins River'), 455, 478, 2, 20, 14),
+    (_('Neustadt'), 219, 216, 2, -25, -14),
+    (_('Pachacan'), 403, 50, 2, -50, 0),
+    (_('Riversdale'), 452, 612, 2, -45, -14),
+    (_('S. Antonio'), 224, 760, 2, -25, -14),
+    (_('San Antonio'), 226, 485, 2, 45, -14),
+    (_('San Estevan'), 390, 131, 2, 45, 14),
+    (_('San Jose'), 369, 120, 2, -50, 0),
+    (_('San Narciso'), 387, 83, 2, -60, 0),
+    (_('San Pedro'), 562, 207, 2, 25, 14),
+    (_('Sarteneja'), 505, 67, 2, 30, 14),
+    (_('Shipyard'), 354, 219, 2, 20, 14),
+    (_('Spanish Lookout'), 231, 424, 2, -30, -14),
+    (_('Valley of Peace'), 287, 404, 2, 0, -14),
+    (_('Yo Creek'), 350, 153, 2, -50, 0)
+]
+
+RIVERS = [
+    (_('Blue Creek River'), 254, 240, 182, -35),
+    (_('Hondo River'), 253, 329, 108, 45),
+    (_('New River'), 252, 372, 217, 60),
+    (_('Bravo River'), 251, 206, 284, 50),
+    (_('Boothis River'), 250, 280, 305, 60),
+    (_('Sibun River'), 249, 348, 449, 60),
+    (_('Belice River'), 248, 274, 412, 30),
+    (_('Macal River'), 247, 232, 510, -80),
+    (_('Raspaculo Branch River'), 246, 303, 588, 30),
+    (_('Sarstoon River'), 245, 195, 864, 0),
+    (_('Moho River'), 244, 212, 819, 0),
+    (_('Deep River'), 243, 309, 726, -40),
+    (_('Grande River'), 242, 266, 738, -40),
+    (_('Monkey River'), 241, 369, 720, -30),
+    (_('Caribbean Sea'), 240, 655, 685, 45),
+    (_('Lake Southern'), 239, 444, 444, 0),
+    (_('Lake Northern'), 238, 447, 394, 0),
+    (_('Lake Shiptesrn'), 237, 499, 108, 45)
+]
+
+ROUTES = []
+
+
