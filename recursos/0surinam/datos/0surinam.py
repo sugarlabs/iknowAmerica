5a0eb4a6893f4dcdf0a1dbdb6f50b307044ffda9
Alan Aguiar <alanjas@hotmail.com>
2012-04-05 03:13:42 -0300

    fix 0surinam to the new system

---
+++ recursos/0surinam/datos/0surinam.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+NAME = _('Surinam')
+
+STATES = [
+    (_('Nickerie'), 254, 261, 152, 0),
+    (_('Coronie'), 253, 355, 150, 0),
+    (_('Saramacca'), 252, 454, 136, 45),
+    (_('Wanica'), 251, 519, 126, 0),
+    (_('Comme-Wijne'), 250, 587, 114, 90),
+    (_('Para'), 249, 527, 177, 0),
+    (_('Marowijne'), 248, 661, 152, 45),
+    (_('Brokopondo'), 247, 540, 294, 90),
+    (_('Sipaliwini'), 246, 384, 468, 0),
+    (_('Brazil'), 245, 488, 816, 0),
+    (_('Guyana'), 244, 93, 630, 0),
+    (_('French Guiana'), 243, 732, 310, 90)
+]
+
+CAPITALS = [
+    (_('Paramaribo'), 532, 113, 0, -50, -14),
+    (_('Albina'), 711, 164, 1, -20, 14),
+    (_('Brokopondo'), 560, 239, 1, 30, -14),
+    (_('Groningen'), 480, 124, 1, -20, 14),
+    (_('Lelydorp'), 524, 133, 1, 40, 0),
+    (_('Nieuw Amsterdam'), 547, 108, 1, 65, -14),
+    (_('Nieuw Nickerie'), 231, 96, 1, 0, -14),
+    (_('Onverwacht'), 528, 151, 1, 15, 14),
+    (_('Totness'), 347, 106, 1, 0, -14)
+]
+
+CITIES = [
+    (_('Anapaike'), 717, 509, 2, -25, 14),
+    (_('Apetina'), 552, 492, 2, 0, -14),
+    (_('Apoera'), 204, 221, 2, 0, 14),
+    (_('Bakhuis'), 265, 290, 2, 0, 14),
+    (_('Benzdorp'), 710, 468, 2, 10, 14),
+    (_('Bitagron'), 383, 223, 2, -15, -14),
+    (_('Boskamp'), 416, 118, 2, -40, 14),
+    (_('Brownsweg'), 536, 249, 2, -15, 14),
+    (_('Cottica'), 688, 439, 2, -20, -14),
+    (_('Kajana'), 470, 432, 2, 0, -14),
+    (_('Kamp 52'), 262, 275, 2, 0, -14),
+    (_('Kwamalasamutu'), 277, 681, 2, 30, -14),
+    (_('Moengo'), 658, 147, 2, 0, -14),
+    (_('Pelelu Tepu'), 448, 552, 2, 10, 14),
+    (_('Pokigron'), 501, 337, 2, -20, 14),
+    (_('Wageningen'), 283, 122, 2, -20, 14),
+    (_('Zanderij'), 526, 177, 2, -15, 14)
+]
+
+RIVERS = [
+    (_('Satamacca River'), 254, 444, 293, 70),
+    (_('Neckerie River'), 253, 268, 235, 35),
+    (_('Coppename River'), 252, 404, 216, 75),
+    (_('Suriname River'), 251, 441, 468, 40),
+    (_('Commewijne River'), 250, 622, 146, -60),
+    (_('Maroni River'), 249, 673, 260, 70),
+    (_('Lawa River'), 248, 679, 449, -60),
+    (_('Oelemari River'), 247, 613, 578, 45),
+    (_('Litani River'), 246, 670, 622, 60),
+    (_('Patoemeu River'), 245, 481, 609, 70),
+    (_('Tapanahony River'), 244, 475, 510, 40),
+    (_('Lucie River'), 243, 256, 511, 30),
+    (_('Kabalebo River'), 242, 148, 370, 50),
+    (_('Sipaliwini River'), 241, 327, 701, -30),
+    (_('Kutari River'), 240, 260, 727, -45),
+    (_('Courantyne River'), 239, 93, 348, 65),
+    (_('Atlantic Ocean'), 238, 477, 41, 0)
+]
+
+ROUTES = []
+
+
