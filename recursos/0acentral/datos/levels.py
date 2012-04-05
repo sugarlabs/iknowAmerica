d2d6a162691b7871538c084d2c0489444322f699
Alan Aguiar <alanjas@hotmail.com>
2012-04-05 17:21:23 -0300

    add levels.py for 0acentral

---
+++ recursos/0acentral/datos/levels.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+LEVEL1 = [
+        _('Countries'),
+        ['lineasDepto'],
+        [],
+[
+    (_('Guatemala'), 1, _('Guatemala'), _('Is west')),
+    (_('Belice'), 1, _('Belice'), _('Is west')),
+    (_('Honduras'), 1, _('Honduras'), _('Is southwest')),
+    (_('El Salvador'), 1, _('El Salvador'), _('Is southwest')),
+    (_('Nicaragua'), 1, _('Nicaragua'), _('Is southwest')),
+    (_('Costa Rica'), 1, _('Costa Rica'), _('Is south')),
+    (_('Panamá'), 1, _('Panamá'), _('Is south')),
+    (_('Cuba'), 1, _('Cuba'), _('Is northwest')),
+    (_('Jamaica'), 1, _('Jamaica'), _('Is northwest')),
+    (_('Haiti'), 1, _('Haiti'), _('Is north')),
+    (_('Dominicana'), 1, _('Dominicana'), _('Is north')),
+    (_('Puerto Rico'), 1, _('Puerto Rico'), _('Is northeast')),
+    (_('Trinidad and Tobago'), 1, _('T. y Tobago'), _('Is east')),
+    (_('the Gran Bahama Island'), 1, _('Is. Gran Bahama'), _('Is northwest')),
+    (_('the Andros Island'), 1, _('Is. Andros'), _('Is northwest')),
+    (_('the Caimán Islands'), 1, _('Is. Caimán'), _('Is northwest')),
+    (_('Martinica'), 1, _('Martinica'), _('Is east')),
+    (_('Dominica'), 1, _('Dominica'), _('Is east')),
+    (_('Guadalupe'), 1, _('Guadalupe'), _('Is northeast')),
+    (_('Antigua and Barbuda'), 1, _('A. y Barbuda'), _('Is northeast')),
+    (_('Saint Lucia'), 1, _('St. Lucia'), _('Is east')),
+    (_('San Vicente and Granadinas'), 1, _('San V. y Granadinas'), _('Is east')),
+    (_('Granada'), 1, _('Granada'), _('Is east')),
+    (_('San Salvador'), 1, _('San Salvador'), _('Is north')),
+    (_('Gran Inagua'), 1, _('Gran Inagua'), _('Is north')),
+    (_('Isla Margarita'), 1, _('Is. Margarita'), _('Is east'))
+]
+]
+
+LEVEL2 = [
+        _('Capitals'),
+        ['lineasDepto', 'capitales'],
+        [],
+[
+    (_('the city of |%s') % _('Basse-Terre'), 2, _('Basse-Terre'), _('Is east')),
+    (_('the city of |%s') % _('Belmopan'), 2, _('Belmopan'), _('Is west')),
+    (_('the city of |%s') % _('Bridgetown'), 2, _('Bridgetown'), _('Is east')),
+    (_('the city of |%s') % _('Castries'), 2, _('Castries'), _('Is east')),
+    (_('the city of |%s') % _('Fort-de-France'), 2, _('Fort-de-France'), _('Is east')),
+    (_('the city of |%s') % _('Guatemala'), 2, _('Guatemala'), _('Is west')),
+    (_('the city of |%s') % _('Kingston'), 2, _('Kingston'), _('Is northwest')),
+    (_('the city of |%s') % _('La Habana'), 2, _('La Habana'), _('Is northwest')),
+    (_('the city of |%s') % _('Managua'), 2, _('Managua'), _('Is southwest')),
+    (_('the city of |%s') % _('Nassau'), 2, _('Nassau'), _('Is north')),
+    (_('the city of |%s') % _('Panamá'), 2, _('Panamá'), _('Is southeast')),
+    (_('the city of |%s') % _('Port of Spain'), 2, _('Port of Spain'), _('Is east')),
+    (_('the city of |%s') % _('Port-au-Prince'), 2, _('Port-au-Prince'), _('Is north')),
+    (_('the city of |%s') % _('Roseau'), 2, _('Roseau'), _('Is east')),
+    (_('the city of |%s') % _('San José'), 2, _('San José'), _('Is south')),
+    (_('the city of |%s') % _('San Juan'), 2, _('San Juan'), _('Is northeast')),
+    (_('the city of |%s') % _('San Salvador'), 2, _('San Salvador'), _('Is southeast')),
+    (_('the city of |%s') % _('Santo Domingo'), 2, _('Santo Domingo'), _('Is north')),
+    (_('the city of |%s') % _("Saint George's"), 2, _("St. George's"), _('Is east')),
+    (_('the city of |%s') % _("Saint John's"), 2, _("St. John's"), _('Is northeast')),
+    (_('the city of |%s') % _('Tegucigalpa'), 2, _('Tegucigalpa'), _('Is southwest'))
+]
+]
+
+LEVEL3 = [
+        _('Cities'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        [],
+[
+    (_('the city of |%s') % _('Basse-Terre'), 2, _('Basse-Terre'), _('Is east')),
+    (_('the city of |%s') % _('Belmopan'), 2, _('Belmopan'), _('Is west')),
+    (_('the city of |%s') % _('Bridgetown'), 2, _('Bridgetown'), _('Is east')),
+    (_('the city of |%s') % _('Castries'), 2, _('Castries'), _('Is east')),
+    (_('the city of |%s') % _('Fort-de-France'), 2, _('Fort-de-France'), _('Is east')),
+    (_('the city of |%s') % _('Guatemala'), 2, _('Guatemala'), _('Is west')),
+    (_('the city of |%s') % _('Kingston'), 2, _('Kingston'), _('Is northwest')),
+    (_('the city of |%s') % _('La Habana'), 2, _('La Habana'), _('Is northwest')),
+    (_('the city of |%s') % _('Managua'), 2, _('Managua'), _('Is southwest')),
+    (_('the city of |%s') % _('Nassau'), 2, _('Nassau'), _('Is north')),
+    (_('the city of |%s') % _('Panamá'), 2, _('Panamá'), _('Is southeast')),
+    (_('the city of |%s') % _('Port of Spain'), 2, _('Port of Spain'), _('Is east')),
+    (_('the city of |%s') % _('Port-au-Prince'), 2, _('Port-au-Prince'), _('Is north')),
+    (_('the city of |%s') % _('Roseau'), 2, _('Roseau'), _('Is east')),
+    (_('the city of |%s') % _('San José'), 2, _('San José'), _('Is south')),
+    (_('the city of |%s') % _('San Juan'), 2, _('San Juan'), _('Is northeast')),
+    (_('the city of |%s') % _('San Salvador'), 2, _('San Salvador'), _('Is southeast')),
+    (_('the city of |%s') % _('Santo Domingo'), 2, _('Santo Domingo'), _('Is north')),
+    (_('the city of |%s') % _("Saint George's"), 2, _("St. George's"), _('Is east')),
+    (_('the city of |%s') % _("Saint John's"), 2, _("St. John's"), _('Is northeast')),
+    (_('the city of |%s') % _('Tegucigalpa'), 2, _('Tegucigalpa'), _('Is southwest')),
+    (_('the city of |%s') % _('Bluefields'), 2, _('Bluefields'), _('Is south')),
+    (_('the city of |%s') % _('Camagüey'), 2, _('Camagüey'), _('Is northwest')),
+    (_('the city of |%s') % _('Cap-Haltien'), 2, _('Cap-Haltien'), _('Is north')),
+    (_('the city of |%s') % _('Cienfuegos'), 2, _('Cienfuegos'), _('Is northwest')),
+    (_('the city of |%s') % _('Colón'), 2, _('Colón'), _('Is south')),
+    (_('the city of |%s') % _('David'), 2, _('David'), _('Is south')),
+    (_('the city of |%s') % _('Flores'), 2, _('Flores'), _('Is west')),
+    (_('the city of |%s') % _('Golfito'), 2, _('Golfito'), _('Is south')),
+    (_('the city of |%s') % _('Grand Turk'), 2, _('Grand Turk'), _('Is north')),
+    (_('the city of |%s') % _('Guantánamo'), 2, _('Guantánamo'), _('Is north')),
+    (_('the city of |%s') % _('Holguín'), 2, _('Holguín'), _('Is northwest')),
+    (_('the city of |%s') % _('La Ceiba'), 2, _('La Ceiba'), _('Is southwest')),
+    (_('the city of |%s') % _('La Palma'), 2, _('La Palma'), _('Is southeast')),
+    (_('the city of |%s') % _('Les Cayes'), 2, _('Les Cayes'), _('Is north')),
+    (_('the city of |%s') % _('Matagalpa'), 2, _('Matagalpa'), _('Is southwest')),
+    (_('the city of |%s') % _('Montego Bay'), 2, _('Montego Bay'), _('Is northwest')),
+    (_('the city of |%s') % _('Pinar del Rio'), 2, _('Pinar del Rio'), _('Is northwest')),
+    (_('the city of |%s') % _('Puerto Barrios'), 2, _('Puerto Barrios'), _('Is west')),
+    (_('the city of |%s') % _('Puerto Cabezas'), 2, _('Puerto Cabezas'), _('Is south')),
+    (_('the city of |%s') % _('Puerto Lempira'), 2, _('Puerto Lempira'), _('Is south')),
+    (_('the city of |%s') % _('Puerto Limón'), 2, _('Puerto Limón'), _('Is south')),
+    (_('the city of |%s') % _('Quetzaltenango'), 2, _('Quetzaltenango'), _('Is west')),
+    (_('the city of |%s') % _('San Pedro Sula'), 2, _('San Pedro Sula'), _('Is west')),
+    (_('the city of |%s') % _('Santiago'), 2, _('Santiago'), _('Is south')),
+    (_('the city of |%s') % _('Santa Clara'), 2, _('Sta. Clara'), _('Is northwest'))
+]
+]
+
+
+
+LEVELS = [LEVEL1, LEVEL2, LEVEL3]
+
