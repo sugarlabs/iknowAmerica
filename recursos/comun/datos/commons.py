a4fb8721c1bdb6cb0ff82e48eb85cb185477b6d7
Alan Aguiar <alanjas@hotmail.com>
2012-04-05 04:40:36 -0300

    add a commons.py file to prefix, sufix..

---
+++ recursos/comun/datos/commons.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+PREFIX = [
+    _('We have to look'),
+    _('We have to find'),
+    _('Now we have to look'),
+    _('We need to find')
+]
+
+SUFIX = [
+    _('You can tell where is it?'),
+    _('Where is it?'),
+    _('Can you show me where is it?'),
+    _('Can you find it?')
+]
+
+CORRECT = [
+    _('Very well!'),
+    _('Brilliant!'),
+    _('You found it!'),
+    _('Yes!')
+]
+
+WRONG = [
+    _('No, this is not'),
+    _('Here it is not'),
+    _('It seems that this is not'),
+    _('That is not')
+]
+
+BYE1 = [
+    _("Now I'm ready|for tomorrow...|I'm going to play...|Thanks for your help!"),
+    _("We made it!|I'm going to play ball...|Bye and thanks|for helping me!"),
+    _("We did it!|I'm ready for tomorrow...I'm playing a while...|Until next time!"),
+
+BYE2 = [
+    _("More luck next|I'll play a while...|Thanks!"),
+    _("The next time will be|I'm going to play ball...|Thanks!"),
+    _("Please try again...|I'm going to play...|Thanks!")
+]
+
+
