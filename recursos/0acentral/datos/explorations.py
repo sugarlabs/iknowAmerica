92f80a925692239899b765a3181e6a4eb4bc0297
Alan Aguiar <alanjas@hotmail.com>
2012-04-05 17:25:32 -0300

    create explorations.py for 0acentral

---
+++ recursos/0acentral/datos/explorations.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+EXP1 = [
+    _('Countries'),
+    ['lineasDepto'],
+    [],
+    ['deptos']
+]
+
+EXP2 = [
+    _('Capitals'),
+    ['lineasDepto', 'capitales'],
+    [],
+    ['capitales']
+]
+
+EXP3 = [
+    _('Cities'),
+    ['lineasDepto', 'capitales', 'ciudades'],
+    [],
+    ['capitales', 'ciudades']
+]
+
+EXPLORATIONS = [EXP1, EXP2, EXP3]
+
