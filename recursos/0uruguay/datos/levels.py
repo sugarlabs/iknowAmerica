63a5fd0e63524ccad7c46b19fcc873d7d6c9034e
Alan Aguiar <alanjas@hotmail.com>
2012-04-05 05:02:31 -0300

    create levels.py for 0uruguay

---
+++ recursos/0uruguay/datos/levels.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+
+LEVEL1 = [
+        [_('Departaments')],
+        ['lineasDepto'],
+        [],
+        [
+    (_('the departament of |%s') % _('Artigas'), 1, _('Artigas'), _("It's easy")),
+    (_('the departament of |%s') % _('Salto'), 1, _('Salto'), _("It's easy")),
+    (_('the departament of |%s') % _('Paysandú'), 1, _('Paysandú'), _("It's easy")),
+    (_('the departament of |%s') % _('Río Negro'), 1, _('Río Negro'), _("It's easy")),
+    (_('the departament of |%s') % _('Rivera'), 1, _('Rivera'), _("It's easy")),
+    (_('the departament of |%s') % _('Tacuarembó'), 1, _('Tacuarembó'), _("It's easy")),
+    (_('the departament of |%s') % _('Soriano'), 1, _('Soriano'), _("It's easy")),
+    (_('the departament of |%s') % _('Colonia'), 1, _('Colonia'), _("It's easy")),
+    (_('the departament of |%s') % _('Florida'), 1, _('Florida'), _("It's easy")),
+    (_('the departament of |%s') % _('Flores'), 1, _('Flores'), _("It's easy")),
+    (_('the departament of |%s') % _('San José'), 1, _('San José'), _("It's easy")),
+    (_('the departament of |%s') % _('Durazno'), 1, _('Durazno'), _("It's easy")),
+    (_('the departament of |%s') % _('Treinta y Tres'), 1, _('Treinta y Tres'), _("It's easy")),
+    (_('the departament of |%s') % _('Cerro Largo'), 1, _('Cerro Largo'), _("It's easy")),
+    (_('the departament of |%s') % _('Rocha'), 1, _('Rocha'), _("It's easy")),
+    (_('the departament of |%s') % _('Lavalleja'), 1, _('Lavalleja'), _("It's easy")),
+    (_('the departament of |%s') % _('Maldonado'), 1, _('Maldonado'), _("It's easy")),
+    (_('the departament of |%s') % _('Canelones'), 1, _('Canelones'), _("It's easy")),
+    (_('the departament of |%s') % _('Montevideo'), 1, _('Montevideo'), _("It's easy"))
+]
+]
+
+
