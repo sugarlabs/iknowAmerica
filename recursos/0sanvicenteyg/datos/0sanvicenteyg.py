5df11bf82fdd32a984acf921cd78616327ea4a43
Alan Aguiar <alanjas@hotmail.com>
2012-04-05 03:08:46 -0300

    fix 0sanvicenteyg to the new system

---
+++ recursos/0sanvicenteyg/datos/0sanvicenteyg.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+NAME = _('Saint Vincent and Grenadines')
+
+STATES = [
+    (_('Saint David'), 254, 176, 173, 60),
+    (_('Carlotte'), 253, 293, 271, 80),
+    (_('Saint Andrew'), 252, 112, 409, 30),
+    (_('Saint Patrick'), 251, 106, 317, 20),
+    (_('Saint George'), 250, 197, 484, 0),
+    (_('Is. Bequia'), 249, 594, 75, 0),
+    (_('Is. Quatre'), 248, 551, 169, 0),
+    (_('Is. Mustique'), 247, 670, 300, 0),
+    (_('Is. Bettowia'), 246, 762, 110, 90),
+    (_('Is. Baliceaux'), 245, 714, 188, 0),
+    (_('Is. Pillories'), 244, 680, 240, 0),
+    (_('Is. Petit Mustique'), 243, 653, 372, 0),
+    (_('Is. Savan'), 242, 627, 422, 0),
+    (_('Is. Petit Canouan'), 241, 518, 452, 0),
+    (_('Is. Canouan'), 240, 437, 564, 0),
+    (_('Is. Mayreau'), 239, 320, 704, 90),
+    (_('Is. Union'), 238, 246, 773, 0),
+    (_('Is. Prune'), 237, 318, 808, 45),
+    (_('Is. Petit Saint Vicent'), 236, 332, 871, 0)
+]
+
+CAPITALS = [
+    (_('Kingstown'), 132, 488, 0, 0, -14),
+    (_('Barrouallie'), 35, 317, 1, 20, -14),
+    (_('Chateaubelair'), 101, 212, 1, 0, -14),
+    (_('Georgetown'), 351, 225, 1, 0, -14),
+    (_('Layou'), 41, 394, 1, 20, -14),
+    (_('Port Elizabeth'), 589, 70, 1, 0, -14)
+]
+
+CITIES = [
+    (_('Ashton'), 249, 780, 2, 0, -14),
+    (_('Biabou'), 306, 404, 2, 0, -14),
+    (_('Calliaqua'), 197, 537, 2, 0, -14),
+    (_('Derrick'), 563, 112, 2, 0, -14),
+    (_('Dovers'), 672, 280, 2, 0, -14),
+    (_('Fancy'), 245, 19, 2, 0, 14),
+    (_('Mespotamia'), 250, 440, 2, 0, 14),
+    (_('Orange Hill'), 344, 148, 2, 0, -14),
+    (_('Stubbs'), 268, 502, 2, 0, -14),
+    (_('Wallibou'), 137, 148, 2, 0, -14)
+]
+
+RIVERS = []
+
+ROUTES = []
+
+
