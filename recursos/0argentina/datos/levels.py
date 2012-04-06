c496ebf5ad7f685860995ae525e894eb1a3ed5ca
Alan Aguiar <alanjas@hotmail.com>
2012-04-06 06:02:55 -0300

    add levels.py in 0argentina

---
+++ recursos/0argentina/datos/levels.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+LEVEL1 = [
+        _('Provinces'),
+        ['lineasDepto'],
+        [],
+[
+    (_('Jujuy'), _('Is north')),
+    (_('Salta'), _('Is north')),
+    (_('Formosa'), _('Is north')),
+    (_('Chaco'), _('Is north')),
+    (_('Misiones'), _('Is northeast')),
+    (_('Catamarca'), _('Is northwest')),
+    (_('Sgo. del Estero'), _('Is north')),
+    (_('La Rioja'), _('Is northwest')),
+    (_('San Juan'), _('Is west')),
+    (_('Mendoza'), _('Is west')),
+    (_('Corrientes'), _('Is northeast')),
+    (_('Santa Fe'), _('Queda en el centro-este')),
+    (_('Córdoba'), _('Is in the center')),
+    (_('San Luis'), _('Is in the center')),
+    (_('Entre Rios'), _('Is east')),
+    (_('Buenos Aires'), _('Is east')),
+    (_('La Pampa'), _('Is in the center')),
+    (_('Río Negro'), _('Is south')),
+    (_('Neuquén'), _('Is west')),
+    (_('Chubut'), _('Is south')),
+    (_('Santa Cruz'), _('Is south')),
+    (_('Tierra del Fuego'), _('Is south')),
+    (_('Tucumán'), _('Is northwest'))
+]
+]
+
+LEVEL2 = [
+        _('Provincial capitals'),
+        ['lineasDepto', 'capitales'],
+        [],
+[
+    (_('Buenos Aires'), _('Is east')),
+    (_('Córdoba'), _('Is in the center')),
+    (_('Catamarca'), _('Is northwest')),
+    (_('Corrientes'), _('Is northeast')),
+    (_('Formosa'), _('Is northeast')),
+    (_('La Plata'), _('Is east')),
+    (_('La Rioja'), _('Is northwest')),
+    (_('Mendoza'), _('Is west')),
+    (_('Neuquén'), _('Is south')),
+    (_('Paraná'), _('Is west')),
+    (_('Posadas'), _('Is northeast')),
+    (_('Río Gallegos'), _('Is south')),
+    (_('Rawson'), _('Is south')),
+    (_('Resistencia'), _('Is northeast')),
+    (_('Salta'), _('Is north')),
+    (_('San Juan'), _('Is west')),
+    (_('San Luis'), _('Is in the center')),
+    (_('San Miguel de Tucumán'), _('Is north')),
+    (_('San Salvador de Jujuy'), _('Is north')),
+    (_('Santa Fe'), _('Is west')),
+    (_('Santa Rosa'), _('Is in the center')),
+    (_('Sgo.del Estero'), _('Is north')),
+    (_('Ushuaia'), _('Is south')),
+    (_('Viedma'), _('Is southeast'))
+]
+]
+
+LEVEL3 = [
+        _('Cities'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        [],
+[
+    (_('Buenos Aires'), _('Is east')),
+    (_('Córdoba'), _('Is in the center')),
+    (_('Catamarca'), _('Is northwest')),
+    (_('Corrientes'), _('Is northeast')),
+    (_('Formosa'), _('Is northeast')),
+    (_('La Plata'), _('Is east')),
+    (_('La Rioja'), _('Is northwest')),
+    (_('Mendoza'), _('Is west')),
+    (_('Neuquén'), _('Is south')),
+    (_('Paraná'), _('Is west')),
+    (_('Posadas'), _('Is northeast')),
+    (_('Río Gallegos'), _('Is south')),
+    (_('Rawson'), _('Is south')),
+    (_('Resistencia'), _('Is northeast')),
+    (_('Salta'), _('Is north')),
+    (_('San Juan'), _('Is west')),
+    (_('San Luis'), _('Is in the center')),
+    (_('San Miguel de Tucumán'), _('Is north')),
+    (_('San Salvador de Jujuy'), _('Is north')),
+    (_('Santa Fe'), _('Is west')),
+    (_('Santa Rosa'), _('Is in the center')),
+    (_('Sgo.del Estero'), _('Is north')),
+    (_('Ushuaia'), _('Is south')),
+    (_('Viedma'), _('Is southeast')),
+    (_('Bahía Blanca'), _('Queda en el centro-este')),
+    (_('Comodoro Rivadavia'), _('Is south')),
+    (_('Curuzú Cuatiá'), _('Is northeast')),
+    (_('Esquel'), _('Is southwest')),
+    (_('Gobernador Gregores'), _('Is south')),
+    (_('Gral. Villegas'), _('Is in the center')),
+    (_('Junín'), _('Is in the center')),
+    (_('Las Heras'), _('Is south')),
+    (_('Mar del Plata'), _('Is east')),
+    (_('Mercedes'), _('Is in the center')),
+    (_('Necochea'), _('Is east')),
+    (_('Puerto Deseado'), _('Is south')),
+    (_('Puerto Santa Cruz'), _('Is south')),
+    (_('Río Cuarto'), _('Is in the center')),
+    (_('Rosario'), _('Is east')),
+    (_('San Carlos de Bariloche'), _('Is southwest')),
+    (_('San Rafael'), _('Is west')),
+    (_('Santo Tomé'), _('Is northeast')),
+    (_('Telén'), _('Is in the center')),
+    (_('Zapala'), _('Is west'))
+]
+]
+
+LEVEL4 = [
+        _('Waterways'),
+        ['lineasDepto', 'rios'],
+        [],
+[
+    (_('Negro River'), _('Is in the center')),
+    (_('Chubut River'), _('Is south')),
+    (_('Chico River'), _('Is south')),
+    (_('Deseado River'), _('Is south')),
+    (_('Colorado River'), _('Is in the center')),
+    (_('Salado River'), _('Is north')),
+    (_('Desaguadero River'), _('Is west')),
+    (_('Paraná River'), _('Is northeast')),
+    (_('Alto Paraná River'), _('Is northeast')),
+    (_('Uruguay River'), _('Is east')),
+    (_('Pilcomayo River'), _('Is north')),
+    (_('Bermejo River'), _('Is north')),
+    (_('Río de la Plata River'), _('Is east')),
+    (_('San Blas Bay'), _('Is east')),
+    (_('San Matias Gulf'), _('Is east')),
+    (_('Atlantic Ocean'), _('Is east')),
+    (_('Pacific Ocean'), _('Is west'))
+]
+]
+
+LEVELS = [LEVEL1, LEVEL2, LEVEL3, LEVEL4]
+
