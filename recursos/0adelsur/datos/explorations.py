53ba8a46354ff8ff2b29b33c1cbe2650fd5029c5
Alan Aguiar <alanjas@hotmail.com>
2012-04-06 04:30:28 -0300

    fix 0adelsur with the levels and explorations

---
+++ recursos/0adelsur/datos/explorations.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+EXP1 = [
+    _('Countries'),
+    ['lineasDepto'],
+    [],
+    ['deptos']
+]
+
+EXP2 = [
+    _('Capitals'),
+    ['lineasDepto', 'capitales'],
+    [],
+    ['capitales']
+]
+
+EXP3 = [
+    _('Cities'),
+    ['lineasDepto', 'capitales', 'ciudades'],
+    [],
+    ['capitales', 'ciudades']
+]
+
+EXP4 = [
+    _('Waterways'),
+    ['rios'],
+    [],
+    ['rios']
+]
+
+
+EXPLORATIONS = [EXP1, EXP2, EXP3, EXP4]
+
