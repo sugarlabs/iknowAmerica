8a0abaa5510d423ab96836a70b2e574cb52e3b4a
Alan Aguiar <alanjas@hotmail.com>
2012-04-06 22:08:44 -0300

    add levels.py for 0belice

---
+++ recursos/0belice/datos/levels.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+LEVEL1 = [
+        _('Districts'),
+        ['lineasDepto'],
+        [],
+[
+    (_('Orange Walk'), _('Is northwest')),
+    (_('Cayo'), _('Is in the center')),
+    (_('Toledo'), _('Is south')),
+    (_('Corozal'), _('Is north')),
+    (_('Belice'), _('Is in the center')),
+    (_('Staan Creek'), _('Is in the center'))
+]
+]
+
+LEVEL2 = [
+        _('Districts capitals'),
+        ['lineasDepto', 'capitales'],
+        [],
+[
+    (_('Belmopan'), _('Is in the center')),
+    (_('Belize'), _('Is east')),
+    (_('Corozal'), _('Is north')),
+    (_('Dangriga'), _('Is east')),
+    (_('Orange Walk'), _('Is north')),
+    (_('Punta Gorda'), _('Is south')),
+    (_('San Ignacio'), _('Is west'))
+]
+]
+
+LEVEL3 = [
+        _('Cities'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        [],
+[
+    (_('Belmopan'), _('Is in the center')),
+    (_('Belize'), _('Is east')),
+    (_('Corozal'), _('Is north')),
+    (_('Dangriga'), _('Is east')),
+    (_('Orange Walk'), _('Is north')),
+    (_('Punta Gorda'), _('Is south')),
+    (_('San Ignacio'), _('Is west')),
+    (_('August Pine Ridge'), _('Is north')),
+    (_('Barranco'), _('Is south')),
+    (_('Benque Viejo del Carmen'), _('Is west')),
+    (_('Bermudian Landing'), _('Is in the center')),
+    (_('Big Creek'), _('Is southeast')),
+    (_('Big Fall'), _('Is south')),
+    (_('Blue Creek Village'), _('Is northwest')),
+    (_('Buena Vista'), _('Is north')),
+    (_('Burrell Boom'), _('Is in the center')),
+    (_('Crooked Tree'), _('Is north')),
+    (_('Gales Point'), _('Is east')),
+    (_('Gallon Jug'), _('Is west')),
+    (_('Guinea Grass'), _('Is north')),
+    (_('Hattieville'), _('Is east')),
+    (_('Hopkins'), _('Is east')),
+    (_('Independence'), _('Is southeast')),
+    (_('Jalacte'), _('Is south')),
+    (_('La Democracia'), _('Is in the center')),
+    (_('Ladyville'), _('Is east')),
+    (_('Libertad'), _('Is north')),
+    (_('Little Belize'), _('Is north')),
+    (_('Maskall'), _('Is northeast')),
+    (_('Middlesex'), _('Is in the center')),
+    (_('Monkey River Town'), _('Is southeast')),
+    (_('Mullins River'), _('Is east')),
+    (_('Neustadt'), _('Is northwest')),
+    (_('Pachacan'), _('Is north')),
+    (_('Riversdale'), _('Is east')),
+    (_('S. Antonio'), _('Is south')),
+    (_('San Antonio'), _('Is west')),
+    (_('San Estevan'), _('Is north')),
+    (_('San Jose'), _('Is north')),
+    (_('San Narciso'), _('Is north')),
+    (_('San Pedro'), _('Is northeast')),
+    (_('Sarteneja'), _('Is north')),
+    (_('Shipyard'), _('Is north')),
+    (_('Spanish Lookout'), _('Is west')),
+    (_('Valley of Peace'), _('Is in the center')),
+    (_('Yo Creek'), _('Is north'))
+]
+]
+
+LEVEL4 = [
+        _('Waterways'),
+        ['lineasDepto', 'rios'],
+        [],
+[
+    (_('Blue Creek River'), _('Is northwest')),
+    (_('Hondo River'), _('Is north')),
+    (_('New River'), _('Is north')),
+    (_('Bravo River'), _('Is west')),
+    (_('Boothis River'), _('Is west')),
+    (_('Sibun River'), _('Is east')),
+    (_('Belice River'), _('Is in the center')),
+    (_('Macal River'), _('Is west')),
+    (_('Raspaculo Branch River'), _('Is in the center')),
+    (_('Sarstoon River'), _('Is south')),
+    (_('Moho River'), _('Is south')),
+    (_('Deep River'), _('Is south')),
+    (_('Grande River'), _('Is south')),
+    (_('Monkey River'), _('Is south')),
+    (_('Caribbean Sea'), _('Is east')),
+    (_('Lake Southern'), _('Is east')),
+    (_('Lake Northern'), _('Is east')),
+    (_('Lake Shiptesrn'), _('Is north'))
+]
+]
+
+LEVELS = [LEVEL1, LEVEL2, LEVEL3, LEVEL4]
+
