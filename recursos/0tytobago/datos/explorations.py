b332d0c262c3f2f18f0612bf131625d055eff303
Alan Aguiar <alanjas@hotmail.com>
2012-04-08 06:31:54 -0300

    add levels, explorations for 0tytobago

---
+++ recursos/0tytobago/datos/explorations.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+EXP1 = [
+    _('Regions'),
+    ['lineasDepto'],
+    [],
+    ['deptos']
+]
+
+EXP3 = [
+    _('Cities'),
+    ['lineasDepto', 'capitales', 'ciudades'],
+    [],
+    ['capitales', 'ciudades']
+]
+
+EXP4 = [
+    _('Waterways'),
+    ['rios'],
+    [],
+    ['rios']
+]
+
+EXP5 = [
+    _('Routes'),
+    ['rutas', 'capitales'],
+    ['capitales'],
+    ['rutas']
+]
+
+EXPLORATIONS = [EXP1, EXP3, EXP4, EXP5]
+
---
+++ recursos/0tytobago/datos/levels.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+LEVEL1 = [
+        _('Regions'),
+        ['lineasDepto'],
+        [],
+[
+    (_('Tobago'), _('Is northeast')),
+    (_('Saint David'), _('Is northeast')),
+    (_('Saint George'), _('Is northwest')),
+    (_('Caron'), _('Is west')),
+    (_('Saint Andrew'), _('Is northeast')),
+    (_('Nariva'), _('Is southeast')),
+    (_('Mayaro'), _('Is southeast')),
+    (_('Victoria'), _('Is southwest')),
+    (_('Saint Patrick'), _('Is southwest'))
+]
+]
+
+LEVEL3 = [
+        _('Cities'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        [],
+[
+    (_('Port of Spain'), _('Is northwest')),
+    (_('Arima'), _('Is north')),
+    (_('Arouca'), _('Is north')),
+    (_('Basse Terre'), _('Is south')),
+    (_('Brighton'), _('Is southwest')),
+    (_('Canaan'), _('Is northeast')),
+    (_('Chaguanas'), _('Is west')),
+    (_('Charlotteville'), _('Is northeast')),
+    (_('Debe'), _('Is southwest')),
+    (_('Fullarton'), _('Is southwest')),
+    (_('Guaico'), _('Is northeast')),
+    (_('Guayaguayare'), _('Is southeast')),
+    (_('La Brea'), _('Is southwest')),
+    (_('Maracas'), _('Is northwest')),
+    (_('Maraval'), _('Is northwest')),
+    (_('Moriah'), _('Is northeast')),
+    (_('Moruga'), _('Is south')),
+    (_('Peñal'), _('Is southwest')),
+    (_('Pierreville'), _('Is southeast')),
+    (_('Plymouth'), _('Is northeast')),
+    (_('Point Fortin'), _('Is southwest')),
+    (_('Rio Claro'), _('Is south')),
+    (_('Roxborough'), _('Is northeast')),
+    (_('Saint Joseph'), _('Is northwest')),
+    (_('San Fernando'), _('Is southwest')),
+    (_('San Francique'), _('Is southwest')),
+    (_('San Juan'), _('Is northwest')),
+    (_('Sangre Grande'), _('Is northeast')),
+    (_('Scarborough'), _('Is northeast')),
+    (_('Siparia'), _('Is southwest')),
+    (_('Tabaquite'), _('Is in the center')),
+    (_('Tableland'), _('Is south')),
+    (_('Toco'), _('Is northeast'))
+]
+]
+
+LEVEL4 = [
+        _('Waterways'),
+        ['lineasDepto', 'rios'],
+        [],
+[
+    (_('Ortoire River'), _('Is southeast')),
+    (_('Oropuche River'), _('Is southwest')),
+    (_('Poole River'), _('Is southeast')),
+    (_('Coparo River'), _('Is west')),
+    (_('Caroni River'), _('Is northwest')),
+    (_('Gulf of Paria'), _('Is west')),
+    (_('Caribbean Sea'), _('Is north')),
+    (_('Atlantic Ocean'), _('Is east'))
+]
+]
+
+LEVELS = [LEVEL1, LEVEL3, LEVEL4]
+
